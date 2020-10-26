1a238a9a3bb6a752c24c50c837d99579d41bc30a
TomoyaWk <wakatomo0606@gmail.com>
2020-10-26 12:47:25 +0900

    ログイン機能調整　新規登録機能の制限

--- portfolio/routes/web.php
+++ portfolio/routes/web.php
@@ -10,12 +10,27 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
+//auth関連
+Route::get('login', 'Auth\LoginController@showLoginForm')->name('login');
+Route::post('login', 'Auth\LoginController@login');
+Route::post('logout', 'Auth\LoginController@logout')->name('logout');
+
+Route::get('/password/reset', 'Auth\ForgotPasswordController@showLinkRequestForm')->name('password.request');
+Route::post('/password/email', 'Auth\ForgotPasswordController@sendResetLinkEmail')->name('password.email');
+Route::get('/password/reset/{token}', 'Auth\ResetPasswordController@showResetForm')->name('password.reset');
+Route::post('/password/reset', 'Auth\ResetPasswordController@reset');
+
+Route::group(['middleware' => ['auth', 'can:admin']], function () {
+    //ユーザー登録（ログイン後のみ許可）
+    Route::get('register', 'Auth\RegisterController@showRegistrationForm')->name('register');
+    Route::post('register', 'Auth\RegisterController@register');
+});
 
 
-Auth::routes();
+//ログイン後の投稿画面表示
+Route::get('/admin', 'AdminController@index')->name('admin');
 
-Route::get('/admin', 'AdminController@index')->name('admin'); //投稿画面表示 
-
+//初期画面表示（vue-routerで以下は設定） 
 Route::get('{any}', function () {
     return view('home');
-})->where('any', '.*'); //初期画面表示 
+})->where('any', '.*');
